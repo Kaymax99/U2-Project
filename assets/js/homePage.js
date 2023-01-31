438910ef2dab9c52b9d4a158eb82d540b481b5bd
Simone Basile <simonebasile199@gmail.com>
2023-01-31 15:57:04 +0100

    Update homePage.js

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -103,7 +103,7 @@ const drawAlbumBuonasera = (albumArray) => {
   if (albumArray.length == 6) {
     for (i = 0; i < albumArray.length; i++) {
       divBuonasera.innerHTML += `<div class="row col-4 p-1"><div class="p-0 custCardLG">
-        <img src="${albumArray[i].cover_small}" class="col-3 p-0" />
+        <img src="${albumArray[i].cover_medium}" class="col-3 p-0" />
         <div class="col-9">${albumArray[i].title}</div></div>`;
     }
   }
