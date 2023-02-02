b819ba99f0c43b5a2d12bbc34c08707dbe0d16ad
Simone Basile <simonebasile199@gmail.com>
2023-02-02 12:14:36 +0100

    Added random playlists to left nav

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -6,15 +6,6 @@ const arrayIDAlbum = [
   100674742, 59853252, 130678282,
 ];
 
-const shuffleArray = (array) => {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-};
-
 shuffleArray(arrayIDAlbum);
 const buonaseraAlbums = [...arrayIDAlbum.slice(0, 6)];
 const selectedAlbumIDs = [...arrayIDAlbum.slice(6)];
