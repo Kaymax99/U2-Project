eb408575a8ea827e1e7b782bcd548e1d81532a67
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-03 16:37:00 +0100

    mobile heart

--- assets/js/homePage.js
+++ assets/js/homePage.js
@@ -394,3 +394,54 @@ window.onload = startDrawingAlbums(selectedAlbumIDs, 5);
 //     })
 //   })
 //   console.log("helloooooooooo")
+
+
+
+
+
+// Variables
+const likeBtn = document.querySelector('.heart-icon');
+// get the value from the HTML element
+let isLiked = false;
+// Functions
+const likeClick = () => {
+  // if the like button hasn't been clicked
+  if (!isLiked) {
+    likeBtn.classList.add('isLiked');
+    isLiked = !isLiked;
+  }
+  // if the like button has been clicked
+  else {
+    likeBtn.classList.remove('isLiked');
+    isLiked = !isLiked;
+  }
+};
+// Event Listeners
+likeBtn.addEventListener('click', likeClick);
+
+
+
+
+
+
+
+// desktop
+// Variables
+const likeBtn1 = document.querySelector('.heart-icon1');
+// get the value from the HTML element
+let isLiked1 = false;
+// Functions
+const likeClick1 = () => {
+  // if the like button hasn't been clicked
+  if (!isLiked) {
+    likeBtn1.classList.add('isLiked');
+    isLiked = !isLiked;
+  }
+  // if the like button has been clicked
+  else {
+    likeBtn1.classList.remove('isLiked');
+    isLiked = !isLiked;
+  }
+};
+// Event Listeners
+likeBtn1.addEventListener('click', likeClick);
